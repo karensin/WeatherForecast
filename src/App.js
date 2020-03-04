5aac768f0930c4aa7f10e8f8063a8b3172d74819
Karen k  sin <karenksin@MacBook-Pro.local>
2020-03-04 12:37:45 -0800

    temp conversation

--- src/App.js
+++ src/App.js
@@ -7,39 +7,51 @@ import 'weather-icons/css/weather-icons.css'
 //api.openweathermap.org/data/2.5/weather?q=London,uk
 const API_Key = "d06db66101a950f6afc113f4474df923";
 
+let location ={
+  city: 'San Francisco'
+}
 
 
 function App() {
+  const [currentTemp,setCurrentTemp] = useState()
   const [city, getCity] = useState();
   const [tempMin, setTempMin] = useState(0);
   const [tempMax, setTempMax] = useState(0);
+  const [description, getDescription] = useState()
 
   useEffect(() => {
     const getWeather = async () => {
 
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_Key}`);
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location.city}&appid=${API_Key}`);
 
       const response = await api_call.json();
       console.log(response)
 
+
       setTempMin(response.main.temp_min)
+      
       setTempMax(response.main.temp_max)
+      
       getCity(response.name)
+     
+      getDescription(response.weather[0].main)
+       
+      setCurrentTemp(response.main.temp)
     }
     getWeather();
   }, []);
-
-  function calCelsius(newTemp) {
-    let cell = Math.floor(newTemp - 273.15);
-    return cell;
-  }
-
+  // const data={
+  //   description={description} 
+  // }
+  
   // console.log(setCity)
   return (
 
     < div className="App" >
       <header className="App-header">
-        <Weather tempMin={tempMin} tempMax={tempMax} city={city} />
+        <Weather tempMin={tempMin} tempMax={tempMax} city={city}
+          description={description} currentTemp={currentTemp}
+         />
       </header>
     </div >
   );
--- src/component/weather.jsx
+++ src/component/weather.jsx
@@ -1,39 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 
+const scales = {
+    KELVIN: 'kelvin', 
+    CELSIUS: 'celsius', 
+    FAHRENHEIT: 'fahrenheit'
+}
 
+const location ={
+    city: undefined 
+}
+ 
 const Weather = props => {
-
+    const {currentTemp,tempMin,tempMax,description} = props 
+    //tempScale is the scale we want to convert to
+    const [tempScale, setTempScale]= useState(scales.FAHRENHEIT) 
+    // const [location, setLocation]= useState(location.city) 
 
     return (
         <div className="container">
             <div className="cards"></div>
             <h1> {props.city} </h1>
-            <h5 className="py-4">
+            <h2 className="py-4">
                 <i className="wi wi-day-sunny" />
-            </h5>
+            </h2>
 
-            <h1 className="py-2">  35 &deg; F </h1>
-            {/* Get Celsius */}
-
-
+            <h1 className="py-2"> {convertTemp(currentTemp)} </h1>
+             
             {/** show max n min temp*/}
-            {minmaxTemp(props.tempMin, props.tempMax)}
+            <span> 
+              <div className="py-2 d-flex justify-content-around">{convertTemp(tempMin)} </div>
+              <div className="py-2 d-flex justify-content-around"> {convertTemp(tempMax)}</div>
+            </span>
 
             {/* Weather description */}
-            <h4 className="py-3">  sunny </h4>
+            <h4 className="py-3">  {description} </h4>
         </div>
     );
+    function convertTemp(temp){
+        let newTemp 
+       if(tempScale===scales.CELSIUS){
+            newTemp=calCelsius(temp) 
+       }
+       if(tempScale===scales.FAHRENHEIT){
+            newTemp=getFahrenheit(temp)
+        }
+        if(tempScale===scales.KELVIN){  
+            newTemp=temp
+        }
+         return (
+            <div>
+                {newTemp}
+             </div>
+        );
+    }
+
+    // function minmaxTemp(min, max) {
+    //     let newMin
+    //     let newMax
+    //     //convert to celcius only when tempScale is celsius 
+    //     if(tempScale===scales.CELSIUS){
+    //         newMin=calCelsius(min)
+    //         newMax=calCelsius(max)
+    //     }
+    //     if(tempScale===scales.FAHRENHEIT){
+    //         newMin=getFahrenheit(min)
+    //         newMax=getFahrenheit(max)
+    //     }
+    //     if(tempScale===scales.KELVIN){
+    //         newMin=min
+    //         newMax=max
+    //     }
+
+    //     return (
+    //         <h3>
+    //             <span className="px-4">{newMin}&deg;</span>
+    //             <span className="px-4">{newMax}&deg;</span>
+    //         </h3>
+    //     );
+
+    // }
+
 }
 export default Weather;
 
+function calCelsius(newTemp) {
+    let cell = Math.floor(newTemp - 273.15);
+    return cell;
+  }
 
-function minmaxTemp(min, max) {
-
-    return (
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
-    );
-
+function getFahrenheit(newTemp){
+    const cell=calCelsius(newTemp)
+    return Math.floor(cell * (9/5) + 32);    
 }
+
