3e9aee0b7d08f300dedc4e76dcd18978a0292e77
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-03-13 20:14:59 -0700

    Updates

---
+++ precache-manifest.154a87667065e3d443d0c4e0651bad56.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592dd64ac561236a5922b656e5572572",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "f0d326c28eb9fb4b6c44",
+    "url": "/WeatherForecast/static/css/2.369b747d.chunk.css"
+  },
+  {
+    "revision": "ba8cc058a8b92eef321c",
+    "url": "/WeatherForecast/static/css/main.d9947d10.chunk.css"
+  },
+  {
+    "revision": "f0d326c28eb9fb4b6c44",
+    "url": "/WeatherForecast/static/js/2.1f052406.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/WeatherForecast/static/js/2.1f052406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8cc058a8b92eef321c",
+    "url": "/WeatherForecast/static/js/main.6687dcad.chunk.js"
+  },
+  {
+    "revision": "d6e0e70f8ec066d0d789",
+    "url": "/WeatherForecast/static/js/runtime-main.1c41d806.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/WeatherForecast/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/WeatherForecast/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/WeatherForecast/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/WeatherForecast/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/WeatherForecast/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
